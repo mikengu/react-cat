98902184dcca678637f20b504a7b3ada1d476f3c
Mike Ngu <mikengu01@gmail.com>
2017-09-11 21:50:11 -0500

    Add component

--- src/index.js
+++ src/index.js
@@ -4,3 +4,50 @@ import ReactDOM from 'react-dom';
 import YoutubeSearch from 'youtube-api-search';
 import VideoList from './components/video_list';
 import VideoDetail from './components/video_detail';
+
+class App extends Component {
+    constructor(props) {
+      super(props);
+  
+      this.state = {
+        videos: [],
+        selectedVideo: null
+      };
+  
+      this.videoSearch("cat video");
+    }
+  
+  
+    videoSearch(term) {
+      YoutubeSearch(
+        {
+          key: API_KEY,
+          term: term
+        },
+        videos => {
+          this.setState({
+            videos,
+            selectedVideo: videos[Math.floor(Math.random() * 50)]
+          });
+        }
+      );
+    }
+  
+    render() {
+      const videoSearch = _.debounce(term => {
+        this.videoSearch(term);
+      }, 400);
+  
+      return (
+        <div>
+          <VideoDetail video={this.state.selectedVideo} />
+          <VideoList
+            onVideoSelect={selectedVideo => this.setState({ selectedVideo })}
+            videos={this.state.videos}
+          />
+        </div>
+      );
+    }
+  }
+  
+  ReactDOM.render(<App />, document.querySelector(".container"));
