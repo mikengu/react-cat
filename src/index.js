28affa7f6a35c5392cc8e83dbaed29483d25c2b3
Mike Ngu <mikengu01@gmail.com>
2017-09-27 08:59:34 -0500

    Separate app component

--- src/index.js
+++ src/index.js
@@ -1,53 +1,15 @@
-import _ from 'lodash';
-import React, { Component } from 'react';
-import ReactDOM from 'react-dom';
-import YoutubeSearch from 'youtube-api-search';
-import VideoList from './components/video_list';
-import VideoDetail from './components/video_detail';
+var React = require('react');
+var ReactDOM = require('react-dom');
+require('./style.css');
+var App = require('./components/App');
 
-class App extends Component {
-    constructor(props) {
-      super(props);
-  
-      this.state = {
-        videos: [],
-        selectedVideo: null
-      };
-  
-      this.videoSearch("cat video");
-    }
-  
-  
-    videoSearch(term) {
-      YoutubeSearch(
-        {
-          key: API_KEY,
-          term: term
-        },
-        videos => {
-          this.setState({
-            videos,
-            selectedVideo: videos[Math.floor(Math.random() * 50)]
-          });
-        }
-      );
-    }
-  
-    render() {
-      const videoSearch = _.debounce(term => {
-        this.videoSearch(term);
-      }, 400);
-  
-      return (
-        <div>
-          <VideoDetail video={this.state.selectedVideo} />
-          <VideoList
-            onVideoSelect={selectedVideo => this.setState({ selectedVideo })}
-            videos={this.state.videos}
-          />
-        </div>
-      );
-    }
-  }
-  
-  ReactDOM.render(<App />, document.querySelector(".container"));
+
+
+ReactDOM.render(
+  <App />,
+  document.getElementById('container')
+);
+
+if (module.hot) {
+  module.hot.accept()
+}
