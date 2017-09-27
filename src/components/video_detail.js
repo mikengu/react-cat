faca0802e47e4dfde96748336f4ee5eb7cebb27b
Mike Ngu <mikengu01@gmail.com>
2017-09-27 08:59:45 -0500

    Remove title and description

--- src/components/video_detail.js
+++ src/components/video_detail.js
@@ -10,13 +10,14 @@ const VideoDetail = ({video}) => {
     const videoID = video.id.videoId;
     const url = `https://www.youtube.com/embed/${videoID}`;
     return (
-        <div className="video-detail col-md-8">
-            <div className="embed-responsive embed-responsive-16by9">
-                <iframe className="embed-responsive-item" src={url}></iframe>
+        <div>
+            <div className="title">
+                <h1 className="">Breaktime. Here's a cat video.</h1>
             </div>
-            <div className="details">
-                <div>{video.snippet.title}</div>
-                <div>{video.snippet.description}</div>
+            <div className="video-detail col-md-7 video">
+                <div className="embed-responsive embed-responsive-16by9">
+                    <iframe className="embed-responsive-item" src={url}></iframe>
+                </div>   
             </div>
         </div>
     );
