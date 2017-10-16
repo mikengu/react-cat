b785e6cfb5336943220f51de269e0d4b6a0652c6
Mike Ngu <mikengu01@gmail.com>
2017-10-15 23:19:17 -0500

    Add button

--- src/components/video_detail.js
+++ src/components/video_detail.js
@@ -3,7 +3,7 @@ import React from 'react';
 const VideoDetail = ({video}) => {
     if (!video) {
         return (
-        <div>Loading...</div>
+        <div className="p-4">Loading...</div>
         );
     }
 
@@ -12,10 +12,13 @@ const VideoDetail = ({video}) => {
     return (
         <div>
             <h1 className="p-4">Breaktime. Here's a cat video.</h1>
-            <div className="video-detail col-md-7 video p-4">
-                <div className="embed-responsive embed-responsive-16by9">
-                    <iframe className="embed-responsive-item" src={url}></iframe>
-                </div>   
+            <div className="d-flex align-items-center">
+                    <div className="video-container col-md-8">
+                        <div className="embed-responsive embed-responsive-16by9">
+                            <iframe className="embed-responsive-item" src={url}></iframe>
+                        </div>   
+                    </div>
+                    <button className="cat-btn">Cat Button</button>
             </div>
         </div>
     );
