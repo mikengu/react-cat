9def81ac97f68480e85575c5b265a56420257388
Mike Ngu <mikengu01@gmail.com>
2017-11-28 17:11:47 -0600

    Remove button form video component

--- src/components/video_detail.js
+++ src/components/video_detail.js
@@ -1,28 +1,22 @@
-import React from 'react';
+import React from "react";
 
-const VideoDetail = ({video}) => {
-    if (!video) {
-        return (
-        <div className="p-4">Loading...</div>
-        );
-    }
+const VideoDetail = ({ video }) => {
+  if (!video) {
+    return <div className="p-4">Loading...</div>;
+  }
 
-    const videoID = video.id.videoId;
-    const url = `https://www.youtube.com/embed/${videoID}`;
-    return (
-        <div>
-            <h1 className="p-4">Breaktime. Here's a cat video.</h1>
-            <div className="d-flex align-items-center">
-                    <div className="video-container col-md-8">
-                        <div className="embed-responsive embed-responsive-16by9">
-                            <iframe className="embed-responsive-item" src={url}></iframe>
-                        </div>   
-                    </div>
-                    <button className="cat-btn">Cat Button</button>
-            </div>
+  const videoID = video.id.videoId;
+  const url = `https://www.youtube.com/embed/${videoID}`;
+  return (
+    <div>
+      <h1 className="p-4">Breaktime. Here's a cat video.</h1>
+      <div className="video-container col-md-8">
+        <div className="embed-responsive embed-responsive-16by9">
+          <iframe className="embed-responsive-item" src={url} />
         </div>
-    );
+      </div>
+    </div>
+  );
 };
 
 export default VideoDetail;
-
